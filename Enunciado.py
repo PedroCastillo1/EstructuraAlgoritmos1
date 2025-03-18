cd6c905f0301ff4d96ce8a6881cb7d9ddeb9a35b
Luquita-DG <lucadigresia01@gmail.com>
2025-03-18 15:22:21 -0400

    Enunciado_TPO

---
+++ Enunciado.py
@@ -0,0 +1,9 @@
+"""
+SISTEMA DE GESTIÓN DE ENVENTOS
+
+Implementar un sistema que gestione eventos utilizando matrices para organizar datos, litas y diccionarios para la administracion de participantes y actividades, manejo de excepciones para validación y recursividad para cálculos de asistencia y participación.
+
+Utilizar archivos para almacenamiento y recuperacion de datos.
+
+
+"""

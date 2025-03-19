d35e1432a84d588873d72ad25c76f7f78fd331ba
Luquita-DG <lucadigresia01@gmail.com>
2025-03-19 12:33:32 -0400

    Pensando el enunciado

--- Enunciado.py
+++ Enunciado.py
@@ -1,11 +1,62 @@
 """
 SISTEMA DE GESTIÓN DE ENVENTOS
 
-Implementar un sistema que gestione eventos utilizando matrices para organizar datos, litas y diccionarios para la administracion de participantes y actividades, manejo de excepciones para validación y recursividad para cálculos de asistencia y participación.
+Implementar un sistema que gestione eventos utilizando matrices para organizar datos, listas y diccionarios para la administracion de participantes y actividades, manejo de excepciones para validación y recursividad para cálculos de asistencia y participación.
 
 Utilizar archivos para almacenamiento y recuperacion de datos.
 
 
 """
 
-print("hola")
+""" 
+ESTRUCTURA DE LA EMPRESA= 
+    - EMPRESA FULANO (nombre de la empresa)
+
+        # Lugares/Salones (Ubicación)
+            - Salones - La Plata
+            - Salones - BSAS
+
+            # Tipos de salones (Tamanio, )
+
+        # Tipo de Evento 
+            - Cumple XV
+            - Casamiento
+            - Evento Social
+            - Evento Corporativo
+            - Congreso
+        
+        # Servicios (Cuenta con servicios para que el evento sea el mejor)
+            - Parking Priva
+            - Seguridad Privada
+            - Corbertura Medica
+            - Grupo Electrogeno
+            - Sistema de Monitoreo
+            - Sistema Contra Incendios
+            - Climatizacion Central
+            - Estudio de contenidos audiovisuales
+            - Suites (Servicio Adicional)
+            - Area de Ambientacion
+            - Laboratorio Tecnologico
+
+        # Servicios a incluir ()
+            - Catering de comida
+                - Menu base
+                - Menu Ejecutivo
+                - Menu Lujoso
+                - Menu Premium
+            - DJ Para la musica del evento
+            - Fotografos
+            - Actividades durante el Evento
+                - Carnaval carioca
+                - Pelea de alhmoadas
+                -
+            - Bartender
+            -
+
+
+        
+
+
+
+
+"""

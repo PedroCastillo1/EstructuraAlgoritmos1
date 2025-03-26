4e81e837be688c4044c39d6fc5c47e01172ba3f4
Luquita-DG <lucadigresia01@gmail.com>
2025-03-25 21:15:59 -0300

    Planteando calendario

---
+++ Luca/Calendario/Fechas.py
@@ -0,0 +1,14 @@
+"""
+    Necesitamos un algoritmo que se encargue de realizar las fechas de eventos a realizarse.
+    
+    Funciomes PRINCIPALES =
+        - CREAR un evento en una fecha (Verificar DISPONIBILIDAD)
+        - ELIMINAR un evento en una fecha (Verificar EXISTENCIA Y Actualizar DISPONIBILIDAD)
+        - BUSCAR un evento si EXISTE
+    
+    Necesitaremos varias funciones como:
+        - Fecha ingresada sea correcta (verificamos rangos de los valores y llamaremos funciones como si es una anio Bisiesto)
+        - Fecha Disponible (Si es una fecha a realizarse, no se permiten fechas ya pasadas, y si esta disponible como para realizar un evento ese dia)
+        - Una Funcion en la que se pueda visualizar con los dias/meses del anio las fechas para eventos
+         
+"""
